763fae12b55e388b4f748238de99fce7c0a5e30e
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-01-19 22:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
-  const firstDate = Date.UTC(date1);
-  const secondDate = Date.UTC(date2);
+var dateDiffInDays = function (date1, date2) {
+  const firstDate = new Date(date1);
+  const secondDate = new Date(date2);
 
-  // Calculate the time difference in milliseconds
-  const timeDiff = Math.abs(secondDate - firstDate);
+  const timeDiff = Math.abs(secondDate.getTime() - firstDate.getTime());
 
-  // Calculate the difference in days
-  const daysDiff = Math.ceil(timeDiff / (86400));
+  const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
   return daysDiff;
-
-	
-
 	
 };
-
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
