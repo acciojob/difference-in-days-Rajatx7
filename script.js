698987c97e9807cdca4168ffae5736525375f588
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-01-19 22:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
-var dateDiffInDays = function (date1, date2) {
+
   const firstDate = new Date(date1);
   const secondDate = new Date(date2);
 
